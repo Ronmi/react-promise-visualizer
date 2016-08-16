a1f6acb91bc8ce3dea934fd77bb8f1a95d8f4707
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-16 12:03:07 +0800

    update binary

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require("./DefaultProvider"));
-__export(require("./PlainTextProvider"));
-__export(require("./Visualizer"));
+__export(require("./src/DefaultProvider"));
+__export(require("./src/PlainTextProvider"));
+__export(require("./src/Visualizer"));
 //# sourceMappingURL=index.js.map
